c8ba907f4d373b3df1f283bb0ce2911ed6b2c81e
KangJungu <KangJungu@JHJG.com>
2016-03-18 11:48:34 +0900

    iOS계산기
    9가 안눌립니

--- Calculation/ViewController.swift
+++ Calculation/ViewController.swift
@@ -77,158 +77,21 @@ class ViewController: UIViewController {
         
     }
     @IBAction func DivisionAction(sender: AnyObject) {
-        //text가 비어있지 않을때만
-        if let _ =  result.text{
-            
-            //dotExist을 초기화
-            dotExist = false;
-            
-            //이전데이터가 있을때
-            if let _ = nowData{
-                //원래 있던것을 beforeData로 옮기고 현재 창에 있는것을 nowData변수에 저장
-                beforeData = nowData
-                nowData = result.text
-                
-                //옵셔널 풀면서 현재 데이터를 int형으로 변환
-                if let beforeTemp = beforeData{
-                    doubleBeforeData = Double(beforeTemp)
-                }
-                if let nowTemp = nowData{
-                    doubleNowData = Double(nowTemp)
-                }
-                
-                //옵셔널 형 다 풀어주고 부호에 따른 계산
-                if let beforeTemp = doubleBeforeData{
-                    if let nowTemp = doubleNowData{
-                        if sign == "+"{
-                            result.text = String(beforeTemp+nowTemp)
-                        }else if sign == "-"{
-                            result.text = String(beforeTemp-nowTemp)
-                        }else if sign == "*"{
-                            result.text = String(beforeTemp*nowTemp)
-                        }else if sign == "/"{
-                            result.text = String(beforeTemp/nowTemp)
-                        }else{
-                            
-                        }
-                    }
-                }
-                signFlag = true;
-            }else{//이전데이터가 없을때
-                //현재 데이터에 result Text에 저장된 값을 넣고
-                nowData = result.text
-                //result Text 비우기
-                result.text = ""
-            }
-            //부호 저장
-            sign = "/"
-            
-            
-        }
+        self.SignMethod("/")
     }
     @IBAction func MultipleAction(sender: AnyObject) {
-        //text가 비어있지 않을때만
-        if let _ =  result.text{
-            
-            //dotExist을 초기화
-            dotExist = false;
-            
-            
-            //이전데이터가 있을때
-            if let _ = nowData{
-                //원래 있던것을 beforeData로 옮기고 현재 창에 있는것을 nowData변수에 저장
-                beforeData = nowData
-                nowData = result.text
-                
-                //옵셔널 풀면서 현재 데이터를 int형으로 변환
-                if let beforeTemp = beforeData{
-                    doubleBeforeData = Double(beforeTemp)
-                }
-                if let nowTemp = nowData{
-                    doubleNowData = Double(nowTemp)
-                }
-                
-                //옵셔널 형 다 풀어주고 부호에 따른 계산
-                if let beforeTemp = doubleBeforeData{
-                    if let nowTemp = doubleNowData{
-                        if sign == "+"{
-                            result.text = String(beforeTemp+nowTemp)
-                        }else if sign == "-"{
-                            result.text = String(beforeTemp-nowTemp)
-                        }else if sign == "*"{
-                            result.text = String(beforeTemp*nowTemp)
-                        }else if sign == "/"{
-                            result.text = String(beforeTemp/nowTemp)
-                        }else{
-                            
-                        }
-                    }
-                }
-                signFlag = true;
-            }else{//이전데이터가 없을때
-                //현재 데이터에 result Text에 저장된 값을 넣고
-                nowData = result.text
-                //result Text 비우기
-                result.text = ""
-            }
-            //부호 저장
-            sign = "*"
-            
-            
-        }
-    }
+        self.SignMethod("*")    }
+    
     @IBAction func MinusAction(sender: AnyObject) {
-        //text가 비어있지 않을때만
-        if let _ =  result.text{
-            
-            //dotExist을 초기화
-            dotExist = false;
-            
-            
-            //이전데이터가 있을때
-            if let _ = nowData{
-                //원래 있던것을 beforeData로 옮기고 현재 창에 있는것을 nowData변수에 저장
-                beforeData = nowData
-                nowData = result.text
-                
-                //옵셔널 풀면서 현재 데이터를 int형으로 변환
-                if let beforeTemp = beforeData{
-                    doubleBeforeData = Double(beforeTemp)
-                }
-                if let nowTemp = nowData{
-                    doubleNowData = Double(nowTemp)
-                }
-                
-                //옵셔널 형 다 풀어주고 부호에 따른 계산
-                if let beforeTemp = doubleBeforeData{
-                    if let nowTemp = doubleNowData{
-                        if sign == "+"{
-                            result.text = String(beforeTemp+nowTemp)
-                        }else if sign == "-"{
-                            result.text = String(beforeTemp-nowTemp)
-                        }else if sign == "*"{
-                            result.text = String(beforeTemp*nowTemp)
-                        }else if sign == "/"{
-                            result.text = String(beforeTemp/nowTemp)
-                        }else{
-                            
-                        }
-                    }
-                }
-                signFlag = true;
-            }else{//이전데이터가 없을때
-                //현재 데이터에 result Text에 저장된 값을 넣고
-                nowData = result.text
-                //result Text 비우기
-                result.text = ""
-            }
-            //부호 저장
-            sign = "-"
-            
-            
-        }
+        self.SignMethod("-")
     }
     @IBAction func PlusAction(sender: AnyObject) {
+        self.SignMethod("+")
+    }
+    //부호 메소드
+    func SignMethod(sendSign: String){
+        print(sendSign)
+        print(result.text)
         //text가 비어있지 않을때만
         if let _ =  result.text{
             
@@ -274,12 +137,14 @@ class ViewController: UIViewController {
                 result.text = ""
             }
             //부호 저장
-            sign = "+"
+            sign = sendSign
             
             
         }
         
+        
     }
+    
     @IBAction func EqualAction(sender: AnyObject) {
         
         //원래 있던것을 beforeData로 옮기고 현재 창에 있는것을 nowData변수에 저장
@@ -431,7 +296,6 @@ class ViewController: UIViewController {
                 signFlag = false;
             }
         }
-        
         if let before = result.text {
             result.text = before + "7"
         }
@@ -450,6 +314,7 @@ class ViewController: UIViewController {
             result.text = before + "8"
         }
     }
+    
     @IBAction func NineAction(sender: AnyObject) {
         //부호로 계산하는 중일때
         if let identyfy = signFlag {
@@ -464,13 +329,6 @@ class ViewController: UIViewController {
             result.text = before + "9"
         }
     }
-    
-    
-//    여기서 클래스 내 변수에 바로 접근하는 방법?
-//    func SignMethod(sendSign: String,sendResult: UILabel){
-//        
-//        
-//    }
     
     
     
